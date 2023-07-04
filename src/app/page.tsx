ed3b2597ba5c24931b7844817b9ac1a69b3a3346
Voids <grzegorz.banaszak95@gmail.com>
2023-07-04 15:15:55 +0200

    Complite add order form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 import OrderDisplay from "@/components/OrderDisplay";
 import { Good } from "@/types/GoodType";
 import { Order } from "@/types/OrderType";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="py-5 flex flex-col">
+      <OrderDisplay
+        order={
+          new Order("21-02-2023", "Brak", "Przyjęte", [
+            new Good("łożysko", "32", 2, "Brak"),
+          ])
+        }
+      />
       <OrderDisplay
         order={
           new Order("21-02-2023", "Brak", "Przyjęte", [
--- src/components/AddOrder.tsx
+++ src/components/AddOrder.tsx
@@ -1,10 +1,35 @@
 "use client";
 import { Good, GoodType } from "@/types/GoodType";
+import moment from "moment";
 import React, { useState } from "react";
 
 const AddOrder = () => {
   const [commoditis, setCommodities] = useState<Array<GoodType>>([]);
   const [commodity, setCommodity] = useState<GoodType>(new Good("", "", 0, ""));
+  const [formDate, setFormDate] = useState<string>("");
+  const [customerName, setCustomerName] = useState<string>("");
+
+  const validForm = (): boolean => {
+    if (commoditis.length > 0) {
+      const reg = new RegExp(
+        /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/
+      );
+
+      if (!reg.test(formDate)) {
+        const currentDate = moment().format("DD-MM-yyyy");
+        setFormDate(currentDate);
+      }
+
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  const submitForm = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    validForm();
+  };
 
   const addToCommoditis = () => {
     if (
@@ -17,13 +42,15 @@ const AddOrder = () => {
     }
   };
   return (
-    <form className="my-3 bg-white">
+    <form className="my-3 bg-white" onSubmit={submitForm}>
       <div className="flex justify-around py-3">
         <label htmlFor="customer">
           <input
             className="text-center p-2.5 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             type="text"
             placeholder="Zamawiający..."
+            value={customerName}
+            onChange={(e) => setCustomerName(e.target.value)}
           />
         </label>
         <label htmlFor="date">
@@ -31,6 +58,8 @@ const AddOrder = () => {
             className="text-center p-2.5 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             type="text"
             placeholder="23-03-2022"
+            value={formDate}
+            onChange={(e) => setFormDate(e.target.value)}
           />
         </label>
       </div>
@@ -89,24 +118,38 @@ const AddOrder = () => {
           <div className="flex items-center">
             <span
               onClick={addToCommoditis}
-              className="p-3 bg-green-800 text-white "
+              className="py-2 px-3 bg-green-800 text-white mb-5 rounded-md self-end"
             >
               +
             </span>
           </div>
         </div>
         <h3 className="text-center py-4 text-2xl">Lista towarów</h3>
-        <ul className="flex justify-evenly">
+        <ul>
           {commoditis.length > 0 &&
             commoditis.map((item, index) => (
-              <li key={index} className="flex justify-evenly w-full">
+              <li
+                key={index}
+                className="text-center grid grid-cols-5 w-full mb-3 p-2.5 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300"
+              >
                 <div>{item.name}</div>
                 <div>{item.quantity}</div>
                 <div>{item.price}</div>
                 <div>{item.description}</div>
+                <div className="bg-red-500 text-white font-bold rounded-md">
+                  -
+                </div>
               </li>
             ))}
         </ul>
+        <div className="flex justify-center">
+          <button
+            type="submit"
+            className="my-2 px-4 py-2 bg-slate-700 text-white"
+          >
+            Zapisz
+          </button>
+        </div>
       </div>
     </form>
   );
